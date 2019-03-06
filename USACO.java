7e7f83e8bdfda7a6edcad1afdb1b1ba6a9e6dd45
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-03-06 10:14:11 -0500

    successfully parsed board into lake[][] array

--- USACO.java
+++ USACO.java
@@ -10,6 +10,8 @@ public class USACO {
     int e = 0;
     int n = 0;
 
+    int[][] land;
+
     try {
       File f = new File(filename);
       Scanner s = new Scanner(f);
@@ -21,11 +23,25 @@ public class USACO {
       c = Integer.parseInt(temp[1]);
       e = Integer.parseInt(temp[2]);
       n = Integer.parseInt(temp[3]);
+
+      land = new int[r][c];
+
+      for (int i=0;i<r;i++) {
+        String line = s.nextLine();
+        temp = line.split(" ");
+        //System.out.println(Arrays.toString(temp));
+        for (int i2=0;i2<c;i2++) {
+          land[i][i2] = Integer.parseInt(temp[i2]);
+        }
+      }
+
+      //System.out.println(Arrays.deepToString(land));
     }catch (FileNotFoundException error) {
       System.out.println("File not found: "+filename);
     }
 
     System.out.println(r+" "+c+" "+e+" "+n);
+    //System.out.println(Arrays.deepToString(land));
     return 0;
 
 
