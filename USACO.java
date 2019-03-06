0d7039e4b64c301808514305794b89f69e173a31
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-03-06 10:18:47 -0500

    try/catch block

--- USACO.java
+++ USACO.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class USACO {
 
-  public static int bronze(String filename) {
+  public static int bronze(String filename) throw{
     int r = 0;
     int c = 0;
     int e = 0;
@@ -12,32 +12,27 @@ public class USACO {
 
     int[][] land;
 
-    try {
-      File f = new File(filename);
-      Scanner s = new Scanner(f);
 
-      String lineOne = s.nextLine();
-      String[] temp = lineOne.split(" ");
+    File f = new File(filename);
+    Scanner s = new Scanner(f);
 
-      r = Integer.parseInt(temp[0]);
-      c = Integer.parseInt(temp[1]);
-      e = Integer.parseInt(temp[2]);
-      n = Integer.parseInt(temp[3]);
+    String lineOne = s.nextLine();
+    String[] temp = lineOne.split(" ");
 
-      land = new int[r][c];
+    r = Integer.parseInt(temp[0]);
+    c = Integer.parseInt(temp[1]);
+    e = Integer.parseInt(temp[2]);
+    n = Integer.parseInt(temp[3]);
 
-      for (int i=0;i<r;i++) {
-        String line = s.nextLine();
-        temp = line.split(" ");
-        //System.out.println(Arrays.toString(temp));
-        for (int i2=0;i2<c;i2++) {
-          land[i][i2] = Integer.parseInt(temp[i2]);
-        }
+    land = new int[r][c];
+
+    for (int i=0;i<r;i++) {
+      String line = s.nextLine();
+      temp = line.split(" ");
+      //System.out.println(Arrays.toString(temp));
+      for (int i2=0;i2<c;i2++) {
+        land[i][i2] = Integer.parseInt(temp[i2]);
       }
-
-      //System.out.println(Arrays.deepToString(land));
-    }catch (FileNotFoundException error) {
-      System.out.println("File not found: "+filename);
     }
 
     System.out.println(r+" "+c+" "+e+" "+n);
