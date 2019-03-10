981c63ef4871ed0cb93823d4765102d7ddbdcae6
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-03-09 22:56:48 -0500

    copied from google groups

---
+++ DerekDriver.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class DerekDriver {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Bronze:");
+            for (int i = 1; i < 6; i++) {
+                File correct = new File("testCases/makelake." + i + ".out");
+                Scanner yes = new Scanner(correct);
+                int yourAnswer = USACO.bronze("testCases/makelake." + i + ".in");
+                int correctAnswer = yes.nextInt();
+                System.out.println(i + " Yours: " + yourAnswer + " Key: " + correctAnswer + " " + (yourAnswer == correctAnswer));
+                yes.close();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+        try {
+            //System.out.println(USACO.silver("testCases/ctravel.2.in"));
+            System.out.println("Silver:");
+            for (int i = 1; i < 6; i++) {
+                File checkPlus = new File("testCases/ctravel." + i + ".out");
+                Scanner A = new Scanner(checkPlus);
+                int yourAnswer = USACO.silver("testCases/makelake." + i + ".in");
+                int correctAnswer = A.nextInt();
+                System.out.println(i + " Yours: " + yourAnswer + " Key: " + correctAnswer + " " + (yourAnswer == correctAnswer));
+                A.close();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+    }
+}
